0d26a4073ff4fe9da847140bbb7a1cf974e3c595
Faaiz Asim <faaizasm@unist.ac.kr>
2021-02-03 14:43:55 +0900

    a

--- bnn-scale-gradbin-lsqb/models/vgg_cifar10_binary.py
+++ bnn-scale-gradbin-lsqb/models/vgg_cifar10_binary.py
@@ -4,55 +4,63 @@ import torchvision.transforms as transforms
 from torch.autograd import Function
 from .binarized_modules import  BinarizeLinear,BinarizeConv2d
 
-
+htan = None
+#htan = nn.Hardtanh(inplace=True)
 
 class VGG_Cifar10(nn.Module):
 
-    def __init__(self, num_classes=1000):
+    def __init__(self, num_classes=10):
         super(VGG_Cifar10, self).__init__()
         self.infl_ratio=3;
         self.features = nn.Sequential(
             BinarizeConv2d(3, 128*self.infl_ratio, kernel_size=3, stride=1, padding=1,
                       bias=True),
             nn.BatchNorm2d(128*self.infl_ratio),
-            nn.Hardtanh(inplace=True),
+            nn.PReLU(),
 
             BinarizeConv2d(128*self.infl_ratio, 128*self.infl_ratio, kernel_size=3, padding=1, bias=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(128*self.infl_ratio),
-            nn.Hardtanh(inplace=True),
+            nn.PReLU(),
+            htan,
 
 
             BinarizeConv2d(128*self.infl_ratio, 256*self.infl_ratio, kernel_size=3, padding=1, bias=True),
             nn.BatchNorm2d(256*self.infl_ratio),
-            nn.Hardtanh(inplace=True),
+            nn.PReLU(),
+            htan,
 
 
             BinarizeConv2d(256*self.infl_ratio, 256*self.infl_ratio, kernel_size=3, padding=1, bias=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(256*self.infl_ratio),
-            nn.Hardtanh(inplace=True),
+            nn.PReLU(),
+            htan,
 
 
             BinarizeConv2d(256*self.infl_ratio, 512*self.infl_ratio, kernel_size=3, padding=1, bias=True),
             nn.BatchNorm2d(512*self.infl_ratio),
-            nn.Hardtanh(inplace=True),
+            nn.PReLU(),
+            htan,
 
 
             BinarizeConv2d(512*self.infl_ratio, 512, kernel_size=3, padding=1, bias=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(512),
-            nn.Hardtanh(inplace=True)
+            nn.PReLU(),
+            htan
 
         )
         self.classifier = nn.Sequential(
             BinarizeLinear(512 * 4 * 4, 1024, bias=True),
             nn.BatchNorm1d(1024),
-            nn.Hardtanh(inplace=True),
+            nn.PReLU(),
+            htan,
             #nn.Dropout(0.5),
             BinarizeLinear(1024, 1024, bias=True),
             nn.BatchNorm1d(1024),
-            nn.Hardtanh(inplace=True),
+            nn.PReLU(),
+            htan,
             #nn.Dropout(0.5),
             BinarizeLinear(1024, num_classes, bias=True),
             nn.BatchNorm1d(num_classes, affine=False),
@@ -66,7 +74,15 @@ class VGG_Cifar10(nn.Module):
             100: {'lr': 1e-4},
             120: {'lr': 5e-5},
             140: {'lr': 1e-5}
-        }
+        }###binary
+        self.regime2 = {
+            0: {'optimizer': 'SGD', 'lr': 1e-2,
+                'weight_decay': 5e-4, 'momentum': 0.9},
+            10: {'lr': 5e-3},
+            15: {'lr': 1e-3, 'weight_decay': 0},
+            20: {'lr': 5e-4},
+            25: {'lr': 1e-4}
+        } ###Full precision
 
     def forward(self, x):
         x = self.features(x)
