be29c1ec931c433df2849942909b20ac27f3d1d4
Faaiz Asim <faaizasm@unist.ac.kr>
2021-02-04 13:33:57 +0900

    a

--- bnn-scale-gradbin-lsqb/main_binary.py
+++ bnn-scale-gradbin-lsqb/main_binary.py
@@ -169,7 +169,8 @@ def main():
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
     logging.info('training regime: %s', regime)
 
-    print(model.Parameters())
+    for p in model.parameters:
+            print(p)
     for epoch in range(args.start_epoch, args.epochs):
         optimizer = adjust_optimizer(optimizer, epoch, regime)
 
