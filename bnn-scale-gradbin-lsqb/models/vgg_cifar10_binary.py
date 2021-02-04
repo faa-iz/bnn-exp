e79928daaaa139f41342653642caaff538dc74c5
Faaiz Asim <faaizasm@unist.ac.kr>
2021-02-04 12:55:08 +0900

    a

--- bnn-scale-gradbin-lsqb/models/vgg_cifar10_binary.py
+++ bnn-scale-gradbin-lsqb/models/vgg_cifar10_binary.py
@@ -68,7 +68,7 @@ class VGG_Cifar10(nn.Module):
             nn.LogSoftmax()
         )
 
-        self.regime = {
+        self.regime1 = {
             0: {'optimizer': 'Adam', 'betas': (0.9, 0.999),'lr': 5e-3},
             40: {'lr': 1e-3},
             80: {'lr': 5e-4},
@@ -76,7 +76,7 @@ class VGG_Cifar10(nn.Module):
             120: {'lr': 5e-5},
             140: {'lr': 1e-5}
         }###binary
-        self.regime2 = {
+        self.regime = {
             0: {'optimizer': 'SGD', 'lr': 1e-2,
                 'weight_decay': 5e-4, 'momentum': 0.9},
             10: {'lr': 5e-3},
