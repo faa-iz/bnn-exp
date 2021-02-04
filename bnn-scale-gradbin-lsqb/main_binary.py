5fcf82aa22f12a80ea36334d8bdceb22e3db74a2
Faaiz Asim <faaizasm@unist.ac.kr>
2021-02-04 13:46:35 +0900

    a

--- bnn-scale-gradbin-lsqb/main_binary.py
+++ bnn-scale-gradbin-lsqb/main_binary.py
@@ -285,7 +285,7 @@ def forward(data_loader, model, criterion, epoch=0, training=True, optimizer=Non
                              phase='TRAINING' if training else 'EVALUATING',
                              batch_time=batch_time,
                              data_time=data_time, loss=losses, top1=top1, top5=top5))
-        print(model.named_parameters()['features.2.alpha'])
+        print(model.state_dict()['features.2.alpha'])
 
     return losses.avg, top1.avg, top5.avg
 
