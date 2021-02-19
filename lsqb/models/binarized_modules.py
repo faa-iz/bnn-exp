9f27a90c9540cfc6b4ce5aeb0d10c1dab1f578ec
Faaiz Asim <faaizasm@unist.ac.kr>
2021-02-19 12:40:55 +0900

    a

--- lsqb/models/binarized_modules.py
+++ lsqb/models/binarized_modules.py
@@ -21,7 +21,7 @@ class LSQbi(Function):
     def forward(self, value, step_size, nbits):
         #print('forward2')
         print('-------------')
-        print(step_size[0])
+        print(step_size.data)
         value  =  value.clamp(-step_size[0],step_size[0])
         self.save_for_backward(value, step_size)
         self.other = nbits
