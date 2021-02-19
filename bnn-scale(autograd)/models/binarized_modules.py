f21aeb4e460943802c99871c0468aa738da6e369
Faaiz Asim <faaizasm@unist.ac.kr>
2021-02-19 13:09:03 +0900

    a

--- bnn-scale(autograd)/models/binarized_modules.py
+++ bnn-scale(autograd)/models/binarized_modules.py
@@ -137,31 +137,29 @@ class BinarizeConv2d(nn.Conv2d):
             #self.beta.data.copy_(torch.ones(self.weight.size(0)).cuda() * init2)
             self.init_state.fill_(1)
 
-
         if input.size(1) != 3:
-            input.data = Binarize().apply(input.data)
-        if not hasattr(self.weight,'org'):
-            self.weight.org=self.weight.data.clone()
-        bw=Binarize().apply(self.weight)
-        #print(self.weight.shape)
+            input = Binarize.apply(input)
+        if not hasattr(self.weight, 'org'):
+            self.weight.org = self.weight.data.clone()
+        bw = Binarize.apply(self.weight)
+        # print(self.weight)
 
-
-
-        #print(self.alpha.shape)
-        #print(bw.shape)
-        #sw = bw
-
-        sw = bw*self.alpha.view(bw.size(0),1,1,1)#*self.beta.view(bw.size(0),1,1,1)
-
-
-        out = nn.functional.conv2d(input, sw, None, self.stride,
+        out = nn.functional.conv2d(input, bw, None, self.stride,
                                    self.padding, self.dilation, self.groups)
 
-
-
-
+        print(out.shape)
+        out = out * self.alpha.view(bw.size(0),1,1,1)
         if not self.bias is None:
-            self.bias.org=self.bias.data.clone()
+            self.bias.org = self.bias.data.clone()
             out += self.bias.view(1, -1, 1, 1).expand_as(out)
+
+
+
+
+
+
+
+
+
 
         return out
