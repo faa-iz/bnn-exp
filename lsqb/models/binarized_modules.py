31a5d103f5ef0c7824b25a8c5e90c9662180cec4
Faaiz Asim <faaizasm@unist.ac.kr>
2021-03-02 23:13:00 +0900

    a

--- lsqb/models/binarized_modules.py
+++ lsqb/models/binarized_modules.py
@@ -63,8 +63,10 @@ class Binarizetact(Function):
         grad_input1 = (1 - torch.pow(torch.tanh(tensor), 2)) * grad_output
 
         tensor =  tensor.clamp(-1,1)
+        high_grad = 1-tensor
+        low_grad = -1-tensor
 
-        grad_input2 =  lower * (-1-tensor) + higher * (1-tensor)
+        grad_input2 =  lower * low_grad + higher * high_grad
         return grad_input1 + grad_input2
 
 
