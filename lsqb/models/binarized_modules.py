b32579cfa2c23acc0491e263fc782374499acb87
Faaiz Asim <faaizasm@unist.ac.kr>
2021-03-02 23:15:21 +0900

    a

--- lsqb/models/binarized_modules.py
+++ lsqb/models/binarized_modules.py
@@ -67,7 +67,7 @@ class Binarizetact(Function):
         low_grad = (-1-tensor)*lower
 
         grad_input2 =  low_grad + higher
-        return grad_input1 + grad_input2
+        return grad_input1# + grad_input2
 
 
 class LSQbi(Function):
